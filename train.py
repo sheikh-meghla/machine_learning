e21d481179fbd5d5f6d31d766b62c8b63c712daf
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-31 16:31:08 +0600

    Refactor training and testing data separation for polynomial regression model

--- train.py
+++ train.py
@@ -5,5 +5,16 @@ numpy.random.seed(2)
 x = numpy.random.normal(3, 1, 100)
 y = numpy.random.normal(150, 40, 100) / x
 
-plt.scatter(x, y)
+train_x = x[:80]
+train_y = y[:80]
+
+test_x = x[80:]
+test_y = y[80:]
+
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+
+myline = numpy.linspace(0, 6, 100)
+
+plt.scatter(train_x, train_y)
+plt.plot(myline, mymodel(myline))
 plt.show()
